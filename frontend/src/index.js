0b07be78a733258f784570e28aec479ef1b04d0e
Lucas Nelson <lucasmnelson2020@gmail.com>
2022-06-01 19:40:23 -0400

    Resolved API key issue and React v17.* bug

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './assets/style/index-compiled.css';
 import App from './App';
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Profile from './components/profile/profile';
 
-
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
 );
